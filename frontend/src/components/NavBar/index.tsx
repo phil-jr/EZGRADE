f3189136b0e1aa0e5cc74be2f11f342d893e6a22
phil-jr <fpoubrejr@gmail.com>
2023-02-09 01:37:07 -0500

    correcting for linting

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,13 +1,8 @@
-import React, { useState } from "react";
 import './index.css'
 
 const NavBar = () => {
-
-    const [testState, setTestState] = useState("A little biy");
-
     return (
         <>
-            <p>{testState}</p>
         </>
     )
 }
