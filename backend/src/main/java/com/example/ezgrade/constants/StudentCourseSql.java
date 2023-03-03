926f36ed326114773e28390c58c45ed3acf80de8
phil-jr <fpoubrejr@gmail.com>
2023-03-03 02:28:44 -0500

    Adding comment placeholder

---
+++ backend/src/main/java/com/example/ezgrade/constants/StudentCourseSql.java
@@ -0,0 +1,11 @@
+package com.example.ezgrade.constants;
+
+public class StudentCourseSql {
+  
+  private StudentCourseSql() {
+    // restrict instantiation
+  }
+
+  public static final String SELECT_STUDENT_COURSE_CARD = "";
+
+}
